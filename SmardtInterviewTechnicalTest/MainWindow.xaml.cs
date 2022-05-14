218e5c310c376aaf71387484938242190c313fec
Hayden <perfecturtle@hotmail.com>
2022-05-14 16:39:35 +1000

    Added the ability to update trend chart

--- SmardtInterviewTechnicalTest/MainWindow.xaml.cs
+++ SmardtInterviewTechnicalTest/MainWindow.xaml.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using SmardtInterviewTechnicalTest.ViewModels;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,15 +23,25 @@ namespace SmardtInterviewTechnicalTest
     /// </summary>
     public partial class MainWindow : Window
     {
+        public MainWindowViewModel MainWindowViewModel => DataContext as MainWindowViewModel;
+
         public MainWindow()
         {
+
             InitializeComponent();
-            TrendChartTemperature.Series[0].Points.Add(3.0).AxisLabel = "Time";
-            TrendChartTemperature.Series[0].Points.AddXY(150, 150);
+            //TrendChartTemperature.Series[0].Points.AddXY(DateTime.Now.Minute, 100);
+
+            //TrendChartTemperature.Series[0].Points.AddXY(DateTime.Now.AddMinutes(10).Minute, 150);
             TrendChartTemperature.Series[0].BorderWidth = 4;
+            TrendChartTemperature.Series[1].BorderWidth = 4;
             TemperatureChartArea.AxisY.Title = "Temperature";
+            TemperatureChartArea.AxisX.Title = "Time (Min)";
+        }
 
-
+        private void UpdateChart_Click(object sender, RoutedEventArgs e)
+        {
+            TrendChartTemperature.Series[0].Points.AddXY(DateTime.Now, MainWindowViewModel.InputTemperature != null ? MainWindowViewModel.InputTemperature : 0);
+            TrendChartTemperature.Series[1].Points.AddXY(DateTime.Now, MainWindowViewModel.TemperatureSetpoint);
         }
     }
 }
